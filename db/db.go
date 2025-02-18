1837c5d61d19f47c97fd388ccd6d1eac9c655f4d
lm-Digambar-Gosavi <digambargir.gosavi@lemmatechnologies.com>
2025-02-18 11:52:27 +0530

    comment add to data

--- db/db.go
+++ db/db.go
@@ -1,25 +1,25 @@
 package db
 
 import (
-	"database/sql"
+	"database/sql" // Provides an interface for database operations
 	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var DB *sql.DB
+var DB *sql.DB // database connection
 
 func ConnectDb() {
 	dsn := "root:root@123@tcp(127.0.0.1:3306)/ecommerce"
 	var err error
-	DB, err = sql.Open("mysql", dsn)
+	DB, err = sql.Open("mysql", dsn) //  initializes a database connection (only validates arguments)
 
 	if err != nil {
-		log.Fatal("Error connting to databse: ", err)
+		log.Fatal("Error connting to databse: ", err) // Logs error message  and exits program
 	}
 
-	err = DB.Ping()
+	err = DB.Ping() // verifies the connection
 	if err != nil {
 		log.Fatal("Database connection failed", err)
 	}
